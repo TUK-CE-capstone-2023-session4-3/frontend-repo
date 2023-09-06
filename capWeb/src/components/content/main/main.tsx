6de9d4d34a26f8796f50c771401c3fc1b40d62ef
devGEP <ju0032564@gmail.com>
2023-09-06 15:41:38 +0900

    fix: 메세지 버튼 수정

--- capWeb/src/components/content/main/main.tsx
+++ capWeb/src/components/content/main/main.tsx
@@ -7,6 +7,10 @@ import {ExclamationTriangleFill} from 'react-bootstrap-icons';
 
 import Toastify from "../../toastify";
 
+import police_icon from '../../../assets/images/police.png';
+import hospital_icon from '../../../assets/images/hospital.png';
+import admin_icon from '../../../assets/images/admin.png';
+
 export default function main() {
   const [to, setTo] = useState("");
   const [from, setFrom] = useState("");
@@ -128,7 +132,7 @@ export default function main() {
           alt="Video"
         />
 
-        <div className="flex flex-col gap-4 items-center justify-items-center">
+        <div className="flex flex-col gap-1 items-center justify-items-center">
           <div>{"balance : " + balance}</div>
           {/* <input
             className="phoneTo"
@@ -136,12 +140,12 @@ export default function main() {
             placeholder="Phone-To"
           /> */}
 
-          <div className="  my-2 mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
+          {/* <div className="  my-2 mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
             <div className="  w-full">
               <input type="search" x-model="input1" className="phoneTo w-full h-12 px-4 py-1 rounded-r-md border border-gray-100 text-gray-800 focus:outline-none"
                   onChange={onChangeTo} value={to} placeholder="Phone-To" />
             </div>
-          </div>
+          </div> */}
 
           {/* <input
             className="phoneFrom"
@@ -149,14 +153,14 @@ export default function main() {
             placeholder="Phone-From"
             /> */}
 
-          <div className="mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
+          {/* <div className="mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
             <div className="  w-full">
               <input type="search" x-model="input2" className="phoneFrom w-full h-12 px-4 py-1 rounded-r-md border border-gray-100 text-gray-800 focus:outline-none"
                   onChange={onChangeFrom} value={from} placeholder="Phone-From" />
             </div>
-          </div>
+          </div> */}
 
-          <div className="mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
+          {/* <div className="mx-auto w-11/12 justify-center flex items-center rounded-md shadow-md">
             <div className="w-full">
               <textarea
                 className="content w-full h-20 px-4 py-2 rounded-r-md border border-gray-100 text-gray-800 focus:outline-none"
@@ -165,9 +169,9 @@ export default function main() {
                 placeholder="Phone-Text"
               />
             </div>
-          </div>
+          </div> */}
 
-          <button
+          {/* <button
             className="flex my-10 p-2 w-48 h-12 bg-light-red rounded-xl text-white items-center justify-center hover:border-transparent transition duration-500 ease-in-out hover:shadow-xl"
             onClick={onClick}
           >
@@ -175,6 +179,42 @@ export default function main() {
               <ExclamationTriangleFill/>
             </div>
             응급상황 신고 버튼!
+          </button> */}
+          <button
+            className="flex my-10 w-56 h-16 bg-light-red rounded-xl text-white items-center justify-center hover:border-transparent transition duration-500 ease-in-out hover:shadow-xl"
+            onClick={onClick}
+          >
+            <div className="flex gap-2.5">
+              <img src={hospital_icon} className='h-7 mx-2 my-3'/>
+              <div className="my-2">
+                병원에 신고! <br/>
+                To : 119
+              </div>
+            </div>
+          </button>
+          <button
+            className="flex my-10 p-2 w-56 h-16 bg-blue-700 rounded-xl text-white items-center justify-center hover:border-transparent transition duration-500 ease-in-out hover:shadow-xl"
+            onClick={onClick}
+          >
+            <div className="flex gap-2.5">
+              <img src={police_icon} className='h-7 mx-2 my-3'/>
+              <div className="my-2">
+                경찰에 신고! <br/>
+                To : 112
+              </div>
+            </div>
+          </button>
+          <button
+            className="flex my-10 p-2 w-56 h-16 bg-slate-700 rounded-xl text-white items-center justify-center hover:border-transparent transition duration-500 ease-in-out hover:shadow-xl"
+            onClick={onClick}
+          >
+            <div className="flex gap-2.5">
+              <img src={admin_icon} className='h-7 mx-2 my-3'/>
+              <div className="my-2">
+                관리자에 신고! <br/>
+                To : 010-xxxx-xxxx
+              </div>
+            </div>
           </button>
         </div>
       </div>
